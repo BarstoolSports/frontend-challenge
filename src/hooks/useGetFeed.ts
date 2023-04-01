3a5397e8d93f29674aab4fd52604c54b15f5aba1
John Datserakis <johndatserakis@gmail.com>
2023-03-31 21:26:19 -0400

    Add polling support to retrieve the latest stories

--- src/hooks/useGetFeed.ts
+++ src/hooks/useGetFeed.ts
@@ -1,57 +1,106 @@
 import { useEffect, useMemo, useState } from 'react';
+import { toast } from 'react-hot-toast';
 import { API_URL } from '../constants/api';
 import { Stories } from '../types/story';
 
+const POLLING_WAIT = 10000; // ms
+
 export const useGetFeed = () => {
   const [loading, setLoading] = useState(true);
+  // Page 0 and 1 return the same stores. Start at 0 to allow incrementPage to handle incrementing.
   const [page, setPage] = useState(0);
   const [feed, setFeed] = useState<Stories>([]);
   const [errored, setErrored] = useState(false);
 
   const incrementPage = async () => {
+    setLoading(true);
     const nextPage = page + 1;
     const url = `${API_URL}?page=${nextPage}`;
 
-    setLoading(true);
-
     try {
       const response = await fetch(url);
-      const json = await response.json();
-      setFeed([...feed, ...json]);
+
+      if (!response.ok) {
+        throw new Error('API Error');
+      }
+
+      const json: Stories = await response.json();
+      // Uncomment to display "new stories" behavoir.
+      // const jsonSliced = json.slice(24);
+      setFeed((feed) => [...feed, ...json]);
+
+      setPage(nextPage); // Only increment page in case of successful call.
     } catch (error) {
       console.error('There was an error with the load more API call.'); // Sentry log
       setErrored(true);
     } finally {
       setLoading(false);
     }
-
-    setPage(nextPage);
   };
 
-  // Used for initial feed call
+  // Retrieve initial feed on load.
+  let initialized = false;
   useEffect(() => {
-    const getInitialFeed = async () => {
-      setLoading(true);
+    if (!initialized) {
+      // Alleviate local dev double-load issue.
+      initialized = true; // eslint-disable-line react-hooks/exhaustive-deps
 
-      try {
-        const response = await fetch(API_URL);
-        const json = await response.json();
-        setFeed(json);
-      } catch (error) {
-        console.error('There was an error with the API call.'); // Sentry log
-        setErrored(true);
-      } finally {
-        setLoading(false);
-      }
+      incrementPage();
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // Instantiate polling for new stories on load.
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const fetchLatestStories = async () => {
+        try {
+          const response = await fetch(API_URL);
+
+          if (!response.ok) {
+            throw new Error('API Error');
+          }
+
+          const json: Stories = await response.json();
+          console.log('here');
+
+          setFeed((feed) => {
+            const newStories = json.filter(
+              ({ id }) => !feed.some((feedItem) => feedItem.id === id)
+            );
+
+            if (!newStories.length) {
+              toast('No new stories found.');
+              return feed;
+            }
+
+            toast.success('New stories added to the top of the list.');
+            return [...newStories, ...feed];
+          });
+        } catch {
+          console.error('There was an error with the load more API call.'); // Sentry log
+          setErrored(true);
+        } finally {
+          setTimeout(fetchLatestStories, POLLING_WAIT);
+        }
+      };
+
+      fetchLatestStories();
+    }, POLLING_WAIT);
+
+    return () => {
+      clearTimeout(timeout);
     };
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-    getInitialFeed();
-  }, []);
+  const isFirstLoad = !feed.length && loading;
+  const noData = !feed.length && !loading && !errored;
 
   return {
     errored,
     feed,
     incrementPage,
+    isFirstLoad,
     loading,
+    noData,
   };
 };
